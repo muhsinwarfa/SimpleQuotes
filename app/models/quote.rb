37f107e4bcd2e245ac30cb96feaa687b9bf02ca9
muhsin <muhsin.warfa@gmail.com>
2016-10-22 17:17:55 +0300

    Added home page, added new controllers

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,6 @@
 class Quote < ActiveRecord::Base
+	validates :body , presence: true
+	def self.get_random_quote
+  		order(“RANDOM()”).first
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :quotes
+
+  get 'home/index' => 'home#index', :as => 'home'
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
